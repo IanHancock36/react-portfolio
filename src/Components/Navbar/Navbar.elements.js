39d08479a7e1901bf550a73ec540bdb61330167a
Ian Hancock <northroadian@gmail.com>
2021-02-22 07:42:09 -0600

    adding navar and styled components for navbar

---
+++ src/Components/Navbar/Navbar.elements.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+
+
+export const Nav = styled.nav `
+background: #101522;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 1.2 rem;
+position: sticky;
+top: 0; 
+`
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Nav} from './Navbar/Navbar.elements'
+
+
+
+const Navbar = () => {
+    return (
+      <>
+      <Nav>
+          Test
+
+      </Nav>
+      </>
+    )
+}
+
+export default Navbar
+
+// arrow function for component
+// when using the styled compontent you pass in what you called it so its a component inside a component? 
