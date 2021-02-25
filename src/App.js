9dd62f89ae7ca551f12dbf69a7773683e1d5f9b4
Ian Hancock <northroadian@gmail.com>
2021-02-25 06:11:38 -0600

    added menu icon onclick that toggles between two icons for menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import React from 'react';
+import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
 import './App.css'
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HEY YOU</h1>
-    </div>
+    <Router>
+      <Navbar />
+    <Switch>
+      <Route path='/'/>
+    </Switch>
+      </Router>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,26 @@
-import React from 'react'
+
+import React, {useState} from 'react'; 
+import {Link} from 'react-router-dom'
+import {FaBars,FaTimes,GiCrystalBars} from 'react-icons/fa'
+import {MdFingerprint} from 'react-icons/md'
+
 
 function Navbar() {
+    
+    const [click, setClick] = useState(false)
+
+    const handleClick = ()=> setClick(!click)
+    
     return (
        <>
        <div className= 'navbar'>
             <div className = "navbar-container container ">
              <Link to='/' className= "navbar-logo">
+                 <MdFingerprint className = 'navbar-icon' />
                 IH
             </Link>
-              <div className = "menu-icon"> 
-
+              <div className = "menu-icon" onClick={handleClick}> 
+              {click ? <FaTimes /> : <FaBars />}
               </div>
             </div> 
        </div>
@@ -19,3 +30,13 @@ function Navbar() {
 }
 
 export default Navbar
+
+// react hooks below putting useState as false makes the click
+
+// start off as false in the terinary operator which makes the FaBars show up first.
+
+// using the handleClick functon and setting it to "not click (!)" basically allows it to toggle.
+
+// using "onClick={handleClick}" in the menue icon calls the handleClick funtion that has the react hooks of click and setclick
+
+// the handleClick will change from the menu bars to the X which is faTimes
